458b652e396c0a442d3a88e6e7d949f663301d2a
fcevallos-lozano <fevalloslozano@gmo@gmail.com>
2023-05-06 19:02:46 -0400

    changed coordinates and lookahead

--- src/pp_pkg/pp_pkg/purepursuit.py
+++ src/pp_pkg/pp_pkg/purepursuit.py
@@ -33,9 +33,9 @@ class PurePursuit(Node):
         # Point 21 is closest from Point 0
         # Point 1 is furthes from Point 0
         # self.waypoints =  [Point(x=0.0,y=0.0,z=0.0), Point(x=0.571194595265405, y=-0.4277145118491421, z=0.0), Point(x=1.1417537280142898, y=-0.8531042347260006,z=0.0), Point(x=1.7098876452457967, y=-1.2696346390611464,z=0.0), Point(x=2.2705328851607995, y=-1.6588899151216996,z=0.0), Point(x=2.8121159420106827, y=-1.9791445882187304,z=0.0), Point(x=3.314589274316711, y=-2.159795566252656,z=0.0), Point(x=3.7538316863009027, y=-2.1224619985315876,z=0.0), Point(x=4.112485112342358, y=-1.8323249172947023,z=0.0), Point(x=4.383456805594431, y=-1.3292669972090994,z=0.0), Point(x=4.557386228943757, y=-0.6928302521681386,z=0.0), Point(x=4.617455513800438, y=0.00274597627737883,z=0.0), Point(x=4.55408382321606, y=0.6984486966257434,z=0.0), Point(x=4.376054025556597, y=1.3330664239172116,z=0.0), Point(x=4.096280073621794, y=1.827159263675668,z=0.0), Point(x=3.719737492364894, y=2.097949296701878,z=0.0), Point(x=3.25277928312066, y=2.108933125822431,z=0.0), Point(x=2.7154386886417314, y=1.9004760368018616,z=0.0), Point(x=2.1347012144725985, y=1.552342808106984,z=0.0), Point(x=1.5324590525923942, y=1.134035376721349,z=0.0), Point(x=0.9214084611203568, y=0.6867933269918683,z=0.0), Point(x=0.30732366808208345, y=0.22955002391894264,z=0.0), Point(x=-0.3075127599907512, y=-0.2301742560363831,z=0.0), Point(x=-0.9218413719658775, y=-0.6882173194028102,z=0.0), Point(x=-1.5334674079795052, y=-1.1373288016589413,z=0.0), Point(x=-2.1365993767877467, y=-1.5584414896876835,z=0.0), Point(x=-2.7180981380280307, y=-1.9086314914221845,z=0.0), Point(x=-3.2552809639439704, y=-2.1153141204181285,z=0.0), Point(x=-3.721102967810494, y=-2.0979137913841046,z=0.0), Point(x=-4.096907306768644, y=-1.8206318841755131,z=0.0), Point(x=-4.377088212533404, y=-1.324440752295139,z=0.0), Point(x=-4.555249804461285, y=-0.6910016662308593,z=0.0), Point(x=-4.617336323713965, y=0.003734984720118972,z=0.0), Point(x=-4.555948690867849, y=0.7001491248072772,z=0.0), Point(x=-4.382109193278264, y=1.3376838311365633,z=0.0), Point(x=-4.111620918085742, y=1.8386823176628544,z=0.0), Point(x=-3.7524648889185794, y=2.1224985058331005,z=0.0), Point(x=-3.3123191098095615, y=2.153588702898333,z=0.0), Point(x=-2.80975246649598, y=1.9712114570096653,z=0.0), Point(x=-2.268856462266256, y=1.652958931009528,z=0.0), Point(x=-1.709001159778989, y=1.2664395490411673,z=0.0), Point(x=-1.1413833971013372, y=0.8517589252820573,z=0.0), Point(x=-0.5710732645795573, y=0.4272721367616211,z=0.0)]
-        num_points = 50
-        x_values = np.linspace(0, 1, num_points)
-        y_values = 0.5 * np.sin(np.pi * x_values)
+        num_points = 25
+        x_values = np.linspace(0, 2.5, num_points)
+        y_values = 0.625 * np.sin(np.pi * x_values/1.25)
 
         waypoints_sine_wave = [Point(x=x_values[i], y=y_values[i], z=0.0) for i in range(num_points)]
 
@@ -59,7 +59,7 @@ class PurePursuit(Node):
         print(f"42: {d_42}")
         """
         
-        self.lookahead_distance = 0.08
+        self.lookahead_distance = 0.15
         self.current_pose = Odometry().pose.pose
         self.nearest_index = 0
         self.last_visited_index = None
@@ -111,6 +111,16 @@ class PurePursuit(Node):
         w_x = [wp.x for wp in self.waypoints]
         w_y = [wp.y for wp in self.waypoints]
         self.passed_values = []
+        final_waypoint = self.waypoints[-1]
+        distance_to_final = sqrt((final_waypoint.x - self.current_pose.position.x) ** 2 + (final_waypoint.y - self.current_pose.position.y) ** 2)
+        self.get_logger().info(f"final dist: {distance_to_final}")
+
+        if distance_to_final <= 0.3:
+            cmd_vel = Twist()
+            cmd_vel.linear.x = 0.0
+            cmd_vel.angular.z = 0.0
+            self.publisher_.publish(cmd_vel)
+            exit(0)
 
         if self.last_visited_index is None:
             dx = [self.current_pose.position.x - icx for icx in w_x] #list comprehension implied by brackets
@@ -139,6 +149,8 @@ class PurePursuit(Node):
                 break  # not exceed goal
             ind += 1
         self.get_logger().info(f"lookahead: {ind}")
+        self.get_logger().info(f"lastone: {self.last_visited_index}")
+
         if ind < len(w_x):
             tx = w_x[ind]
             ty = w_y[ind]
@@ -152,18 +164,13 @@ class PurePursuit(Node):
         self.get_logger().info(f"angle2: {self.current_pose}")
 
         cmd_vel = Twist()
-        cmd_vel.linear.x = .1  # tune this value
-        cmd_vel.angular.z = 1.1 * alpha  # tune this value
+        cmd_vel.linear.x = .2  # tune this value
+        cmd_vel.angular.z = 1.0 * alpha  # tune this value
 
             # check if the robot is close to the final waypoint
-        final_waypoint = self.waypoints[-1]
-        distance_to_final = sqrt((final_waypoint.x - self.current_pose.position.x) ** 2 + (final_waypoint.y - self.current_pose.position.y) ** 2)
+        #final_waypoint = self.waypoints[-1]
+        #distance_to_final = sqrt((final_waypoint.x - self.current_pose.position.x) ** 2 + (final_waypoint.y - self.current_pose.position.y) ** 2)
 
-            # stop condition (tune the threshold value)
-        stop_threshold = 3.0
-        if distance_to_final < stop_threshold:
-                cmd_vel.linear.x = 0.0
-                cmd_vel.angular.z = 0.0
 
         self.publisher_.publish(cmd_vel)
                 
